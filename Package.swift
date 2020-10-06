3b2411795e34d99ce2cb9af8f43bbb087d8d66ee
Kyle Jessup <kyle.jessup@treefrog.ca>
2020-10-06 11:58:37 -0400

    Dep version fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		.package(url: "https://github.com/kjessup/SAuthCodables.git", .branch("master")),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-CRUD.git", from: "2.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-NIO.git", .branch("master")),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", from: "4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", from: "5.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "5.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", from: "5.0.0"),
 	],
